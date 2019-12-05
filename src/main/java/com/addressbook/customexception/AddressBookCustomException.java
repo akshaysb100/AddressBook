faa30003652b02df569ae851499ed94da0ceaf40
akshaysb100 <akshaybavalekar100@gmail.com>
2019-12-05 17:04:38 +0530

    Add:Create custom exception class

---
+++ src/main/java/com/addressbook/customexception/AddressBookCustomException.java
@@ -0,0 +1,25 @@
+package com.addressbook.customexception;
+
+public class AddressBookCustomException extends  Exception {
+
+    public enum ExceptionType {
+        NO_SUCH_FILE
+    }
+
+    public ExceptionType type;
+
+    public AddressBookCustomException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+
+    public AddressBookCustomException(ExceptionType type, Throwable cause) {
+        super(cause);
+        this.type = type;
+    }
+
+    public AddressBookCustomException(ExceptionType type, String message, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+}
