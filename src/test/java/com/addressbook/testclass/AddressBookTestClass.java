28417c932407b50141ad57e1104c6d867178e2c0
akshaysb100 <akshaybavalekar100@gmail.com>
2019-12-05 16:58:58 +0530

    Add:Create test case for create new address book

--- src/test/java/com/addressbook/testclass/AddressBookTestClass.java
+++ src/test/java/com/addressbook/testclass/AddressBookTestClass.java
@@ -1,5 +1,18 @@
 package com.addressbook.testclass;
 
+import com.addressbook.services.AddressBookServicesImplementation;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class AddressBookTestClass {
 
+    AddressBookServicesImplementation addressBookServices = new AddressBookServicesImplementation();
+
+    @Test
+    public void check_WhenAddressBook_Created_ReturnTrue() throws IOException {
+
+        Assert.assertEquals(true,addressBookServices.createAddressBook( "/home/user/IdeaProjects/AddressBookProject/src/test/java/com/addressbook/jesonfile/newaddressbook.json"));
+    }
 }
